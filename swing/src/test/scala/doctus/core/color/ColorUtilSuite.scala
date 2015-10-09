73d299874901bc9f3845f06614f59f0589e3bb5a
wwagner4 <wwagner4@gmail.com>
2015-10-09 08:32:41 +0200

    fixed color util problem. hsv2rgb

--- swing/src/test/scala/doctus/core/color/ColorUtilSuite.scala
+++ swing/src/test/scala/doctus/core/color/ColorUtilSuite.scala
@@ -27,13 +27,24 @@ color holder 100 DoctusColorRgb(203,0,255) -> DoctusColorRgb(199,0,255)
 
     "Color Util" - {
       "rgb to hsv" - {
-        val r = 0
-        val g = 0
-        val b = 0
-        val is = DoctusColorUtil.rgb2hsv(r, g, b)
-        val should = (2, 3, 4)
-        assert(is == should)
-        assert(1 > 2)
+        * - {
+          val r = 255
+          val g = 0
+          val b = 102
+          val is = DoctusColorUtil.rgb2hsv(r, g, b)
+          val should = (-24, 100, 100)
+          assert(is == should)
+        }
+      }
+      "hsv to rgb" - {
+        * - {
+          val r = -24
+          val g = 100
+          val b = 100
+          val is = DoctusColorUtil.hsv2rgb(r, g, b)
+          val should = (255, 0, 102)
+          assert(is == should)
+        }
       }
     }
   }
