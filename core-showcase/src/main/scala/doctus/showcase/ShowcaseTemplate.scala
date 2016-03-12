7f028cbc4ea95dbfc3e15136798db745aa9d8f59
wwagner4 <wwagner4@gmail.com>
2016-03-12 13:58:33 +0100

    added showcase still image

---
+++ core-showcase/src/main/scala/doctus/showcase/ShowcaseTemplate.scala
@@ -0,0 +1,46 @@
+package doctus.showcase
+
+import doctus.core.template.DoctusTemplate
+import doctus.core.DoctusGraphics
+import doctus.core.DoctusCanvas
+import doctus.core.util.DoctusPoint
+import doctus.core.util.DoctusVector
+import doctus.core.color.DoctusColorBlue
+import java.util.Random
+import doctus.core.color.DoctusColorGreen
+import doctus.core.DoctusColor
+import doctus.core.color.DoctusColorUtil
+import doctus.core.color.DoctusColorRgb
+import doctus.core.color.DoctusColorRgb
+
+case class DoctusTemplateStillImage(canvas: DoctusCanvas) extends DoctusTemplate {
+
+  val ran = new Random
+
+  // None means there is no animation. The draw method is called ones
+  override def frameRate = None
+
+  def draw(g: DoctusGraphics): Unit = {
+
+    val w = canvas.width
+    val h = canvas.width
+    
+    def ranColor: DoctusColor = {
+      val hue = ran.nextInt(360)
+      val (r, g, b) = DoctusColorUtil.hsv2rgb(hue, 100, 100)
+      DoctusColorRgb(r, g, b)
+    }
+
+    for (i <- 1 to 1000) {
+      g.fill(ranColor, 20)
+      g.stroke(DoctusColorBlue, 5)
+      g.strokeWeight(3)
+      g.ellipse(DoctusPoint(ran.nextInt(w), ran.nextInt(h)), 100, 100)
+    }
+  }
+
+  def pointableDragged(pos: DoctusPoint): Unit = () // Nothing to do in a still image
+  def pointablePressed(pos: DoctusPoint): Unit = () // Nothing to do in a still image
+  def pointableReleased(pos: DoctusPoint): Unit = () // Nothing to do in a still image
+
+}
---
+++ swing-showcase/src/main/scala/doctus/swing/ShowcaseTemplateStillImage.scala
@@ -0,0 +1,31 @@
+package doctus.swing
+
+import doctus.showcase.DoctusTemplateStillImage
+import javax.swing.JFrame
+import javax.swing.ImageIcon
+import java.awt.BorderLayout
+import java.awt.Dimension
+import doctus.core.template.DoctusTemplateControllerImpl
+
+object ShowcaseTemplateStillImage extends App {
+
+  val p = DoctusComponentFactory.component
+  val canvas = DoctusTemplateCanvasSwing(p)
+  val sched = DoctusSchedulerSwing
+  val logo = DoctusImageSwing("logo.png")
+
+  val top = new JFrame()
+  top.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
+  top.setTitle("Canvas Showcase")
+  val url = getClass().getClassLoader().getResource("logo.png")
+  if (url != null) top.setIconImage(new ImageIcon(url).getImage)
+
+  top.getContentPane.add(p, BorderLayout.CENTER)
+  top.setSize(new Dimension(900, 700))
+  top.setVisible(true)
+
+  val templ = DoctusTemplateStillImage(canvas)
+  DoctusTemplateControllerImpl(templ, sched, canvas)
+
+
+}
