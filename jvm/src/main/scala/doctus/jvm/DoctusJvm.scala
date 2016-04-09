2af02c6bb3ca6c79dbf913e7a6b0a60fd9da905d
wwagner4 <wwagner4@gmail.com>
2016-04-09 21:12:03 +0200

    name refactoring

--- jvm/src/main/scala/doctus/jvm/DoctusJvm.scala
+++ jvm/src/main/scala/doctus/jvm/DoctusJvm.scala
@@ -13,10 +13,10 @@ case object DoctusSchedulerJvm extends DoctusScheduler {
     require(initialDelay >= 0, "Initial delay must be greater equal to zero. " + duration)
 
     val scheduler: ScheduledExecutorService = Executors.newScheduledThreadPool(1)
-    val b = new Runnable {
+    val runnable = new Runnable {
       override def run(): Unit = f()
     }
-    val future = scheduler.scheduleAtFixedRate(b, initialDelay, duration, TimeUnit.MILLISECONDS)
+    val future = scheduler.scheduleAtFixedRate(runnable, initialDelay, duration, TimeUnit.MILLISECONDS)
 
     new DoctusScheduler.Stopper {
       // Stops the execution of a Scheduler
