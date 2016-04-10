d852ae8c7b337f6803b69fcfbbe1a6b3e0344ceb
wwagner4 <wwagner4@gmail.com>
2016-04-10 13:40:10 +0200

    scheduler start stop works somehow

---
+++ showcase-jvm/src/main/scala/doctus/jvm/ShowcaseJvmSchedulerStop.scala
@@ -0,0 +1,87 @@
+package doctus.jvm
+
+import doctus.core._
+import java.util.ArrayList
+import java.util.List
+import java.util.Random
+import javafx.application.Application
+import javafx.scene._
+import javafx.stage.Stage
+import javafx.scene.canvas.Canvas
+import javafx.application._
+import javafx.scene._
+import javafx.scene.paint._
+import javafx.stage.Stage
+import javafx.stage.WindowEvent
+import javafx.event.EventHandler
+import javafx.event.ActionEvent
+import javafx.event.Event
+import javafx.scene.control.Label
+import javafx.scene.layout.Pane
+import javafx.scene.control.Button
+import javafx.scene.control.ComboBox
+import javafx.scene.control.TextField
+import javafx.scene.layout.FlowPane
+import javafx.scene.layout.VBox
+import javafx.geometry.Insets
+import javafx.geometry.Pos
+import javafx.scene.layout.BorderPane
+
+object ShowcaseJvmSchedulerStop extends App {
+
+  Application.launch(classOf[FxApp], args: _*);
+
+  class FxApp extends Application {
+
+    override def start(stage: Stage) {
+
+      val width = 700
+      val height = 500
+
+      val buttonStart = new Button("START")
+      val buttonStop = new Button("STOP")
+
+      val canvas  = new Canvas()
+      canvas.setWidth(width)
+      canvas.setHeight(height)
+
+      
+      val contPane = new BorderPane()
+      contPane.setPadding(new Insets(20, 20, 20, 20))
+      val compsPane = new FlowPane();
+      compsPane.setAlignment(Pos.CENTER)
+
+      compsPane.getChildren.add(buttonStart)
+      compsPane.getChildren.add(buttonStop)
+      
+      contPane.setTop(compsPane)
+      contPane.setCenter(canvas)
+      
+      val bgCol = Color.WHITE;
+      val scene = new Scene(contPane, width, height, bgCol);
+
+      val doctStart = DoctusActivatableFx(buttonStart)
+      val doctStop = DoctusActivatableFx(buttonStop)
+      val doctCanvas = DoctusCanvasFx(canvas)
+      val doctSched = DoctusSchedulerJvm
+
+      // Start the controller
+      DoctusControllerSchedulerStop(doctSched, doctStart, doctStop, doctCanvas)
+
+      stage.setScene(scene);
+
+      stage.show();
+
+      def handler[T <: Event](h: (T => Unit)): EventHandler[T] =
+        new EventHandler[T] {
+          override def handle(event: T): Unit = h(event)
+        }
+
+      // TODO Find a better solution to exit
+      stage.setOnCloseRequest(handler(e => System.exit(0)))
+
+    }
+  }
+
+}
+
