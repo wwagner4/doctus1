aaf8b1dd9630ec90d424747ffc99ee5e167d6ca7
wwagner4 <wwagner4@gmail.com>
2016-08-13 17:25:34 +0200

    Added WrongPintPosition

---
+++ showcase-core/src/main/scala/doctus/core/ShowcaseWrongPointPosition.scala
@@ -0,0 +1,30 @@
+package doctus.core
+
+import doctus.core.color.{DoctusColorBlack, DoctusColorYellow}
+import doctus.core.template._
+import doctus.core.util._
+
+case class DoctusTemplateWrongPointPosition(canvas: DoctusCanvas) extends DoctusTemplate {
+
+  var pos = Option.empty[DoctusPoint]
+
+  override def frameRate = Some(5)
+
+  def draw(g: DoctusGraphics): Unit = {
+    g.noStroke()
+    g.fill(DoctusColorYellow, 255)
+    g.rect(0, 0, canvas.width, canvas.height)
+    g.stroke(DoctusColorBlack, 255)
+    g.fill(DoctusColorBlack, 255)
+    g.textSize(35)
+    pos.foreach(p => g.text(p.toString, DoctusPoint(5, 40), 0))
+  }
+  def keyPressed(code: DoctusKeyCode): Unit = ()
+  
+  def pointableDragged(pos: DoctusPoint): Unit = ()
+  def pointablePressed(pos: DoctusPoint): Unit = {
+    this.pos = Some(pos)
+  }
+  def pointableReleased(pos: DoctusPoint): Unit = ()
+
+}
---
+++ showcase-scalajs/src/main/scala/doctus/scalajs/ShowcaseWrongPointPosition.scala
@@ -0,0 +1,28 @@
+package doctus.scalajs
+
+import doctus.core.{DoctusTemplateStillImage, DoctusTemplateWrongPointPosition}
+import doctus.core.template.DoctusTemplateController
+import org.scalajs.dom
+import org.scalajs.dom.raw.HTMLCanvasElement
+
+import scala.scalajs.js.annotation.JSExport
+
+@JSExport("WrongPointPosition")
+object ShowcaseWrongPointPosition {
+
+  @JSExport
+  def main() {
+
+    // Get element from html page
+    val canvasElem: HTMLCanvasElement = dom.document.getElementById("canvas").asInstanceOf[HTMLCanvasElement]
+
+    // Create doctus wrappers
+    val templCanvas = DoctusTemplateCanvasScalajs(canvasElem)
+    val sched = DoctusSchedulerScalajs
+
+    // call the controller
+    val templ = DoctusTemplateWrongPointPosition(templCanvas)
+    DoctusTemplateController(templ, sched, templCanvas)
+  }
+
+}
